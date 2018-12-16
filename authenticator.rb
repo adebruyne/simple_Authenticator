b8c92b6080489c477ad740eaee803bb2cbd51330
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-16 14:37:37 -0500

    checks for 4 attempts in while loop

--- authenticator.rb
+++ authenticator.rb
@@ -11,3 +11,12 @@ puts "Welcome to the authenticator"
 25.times {print "-"}
 puts "This program will take input from the user and compare password"
 puts "If password is correct, you will get back the user object."
+
+
+attempts = 1
+while attempts < 4
+    
+    attempts += 1
+
+
+end
