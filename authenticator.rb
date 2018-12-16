1bb74c1de94e68ee5e3571925ca2076498b77949
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-16 14:34:34 -0500

    lays out initial file for project

---
+++ authenticator.rb
@@ -0,0 +1,13 @@
+users = [
+    { username: "aylin", password: "password1"},
+    { username: "jeff", password: "password2"},
+    { username: "bruce", password: "password3"},
+    { username: "beth", password: "password4"},
+    { username: "santa", password: "password5"}
+    
+]
+
+puts "Welcome to the authenticator"
+25.times {print "-"}
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object."
